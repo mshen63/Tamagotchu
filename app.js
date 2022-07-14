51ef8fb91b8875393ef9f2e6adcc623988fabe16
mshen63 <mshen63@gatech.edu>
2022-07-14 08:09:31 -0700

    Add ENV

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-var dev_db_url = ;
+var dev_db_url = ENV['DB_URL'];
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
